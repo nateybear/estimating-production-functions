3e4c9eb3a779903d05b6c90e43ff31d93b8aec8d
Nathan Hattersley <nate.hattersley@gmail.com>
2020-10-22 12:58:27 -0500

    adding OP estimation

--- generateMeasureError.m
+++
@@ -1,5 +0,0 @@
-function data = generateMeasureError(data, globals)
-    groupMeans = mean(data.lnIntermedInput, 1);
-    withinvar = mean((data.lnIntermedInput - groupMeans).^2, 'all');
-    data.lnIntermedInput = data.lnIntermedInput + globals.measureError * sqrt(withinvar) * randn(size(data.lnIntermedInput));
-end
