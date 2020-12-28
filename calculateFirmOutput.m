d7ad1bad768eb75a28718746d81a3b290889a0f0
Nathan Hattersley <nate.hattersley@gmail.com>
2020-12-28 17:51:18 -0600

    refactoring code for readability

--- calculateFirmOutput.m
+++
@@ -1,4 +0,0 @@
-function data = calculateFirmOutput(data, globals)
-    data.lnOutput = globals.beta0 + globals.betaL * data.lnLabor + ...
-        globals.betaK * data.lnKapital + data.omegaT + data.epsilon;
-end
--- keepLastN.m
+++
@@ -1,7 +0,0 @@
-% drop all but the last globals.nperiodsKeep observations from our
-% generated data
-function output = keepLastN(output, globals)
-    for field = string(fieldnames(output)).'
-        output.(field) = output.(field)((end-globals.nperiodsKeep+1):end,:); 
-    end
-end
